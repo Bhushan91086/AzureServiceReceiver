f17bf1e4b030b660bf3b914f4af01c3c12fe9767
Bhushan91086 <bhushanarvekar@gmail.com>
2023-08-07 15:03:47 +0530

    Dead Letter Queue Demo

--- AzureServiceReceiver/Program.cs
+++ AzureServiceReceiver/Program.cs
@@ -5,20 +5,17 @@ namespace AzureServiceReceiver
 {
     internal class Program
     {
-        private static string connection_string = "Endpoint=sb://sbnamespace1986.servicebus.windows.net/;SharedAccessKeyName=Listen;SharedAccessKey=qTB79C4i3plSnjygpbvVxkT9idDciaUOo+ASbJBD6XM=;EntityPath=appqueue";
-        private static string queue_name = "appqueue";
+        private static string connection_string = "Endpoint=sb://sbnamespace1986.servicebus.windows.net/;SharedAccessKeyName=Listen;SharedAccessKey=qTB79C4i3plSnjygpbvVxkT9idDciaUOo+ASbJBD6XM=;EntityPath=appqueue/$DeadLetterQueue";
+        private static string queue_name = "appqueue/$DeadLetterQueue";
 
         static void Main(string[] args)
         {
             ServiceBusClient client = new ServiceBusClient(connection_string);
             ServiceBusReceiver receiver = client.CreateReceiver(queue_name, new ServiceBusReceiverOptions() { ReceiveMode = ServiceBusReceiveMode.PeekLock});
-            var messages = receiver.ReceiveMessagesAsync(3).GetAwaiter().GetResult();
-            foreach (var message in messages)
-            {
-                Console.WriteLine(message.SequenceNumber);
-                Console.WriteLine(message.Body.ToString());
-                receiver.CompleteMessageAsync(message);
-            }
+            var message = receiver.ReceiveMessageAsync().GetAwaiter().GetResult();
+            Console.WriteLine($"Dead Letter Reason: {message.DeadLetterReason}");
+            Console.WriteLine($"Dead Letter Error Description: {message.DeadLetterErrorDescription}");
+            Console.WriteLine(message.Body);
 
             Console.ReadKey();
         }
